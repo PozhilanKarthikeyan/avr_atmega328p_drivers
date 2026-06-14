86299ed6b0730559ad0866375463fb765f9f7148
PozhilanKarthikeyan <pozhilankarthikeyan2005@gmail.com>
2026-06-14 10:50:43 +0530

    Implement GPIO interrupt handling and configuration support. Updated deinit state to input pullup as recommended in trm. refactored  drv_gpio_get_state function to read only pin register for both output and input.

--- drivers/gpio/gpio.c
+++ drivers/gpio/gpio.c
@@ -3,7 +3,71 @@
 static volatile uint8_t* const DDR_ARR[]={&DDRB,&DDRC,&DDRD};
 static volatile uint8_t* const PORT_ARR[]={&PORTB,&PORTC,&PORTD};
 static volatile uint8_t* const PIN_ARR[]={&PINB,&PINC,&PIND};
+static volatile uint8_t* const PCMSK_ARR[]={&PCMSK0,&PCMSK1,&PCMSK2};
+
 static int const MAX_PORT_PIN_NUMBER[]={MAX_PORTB_PIN_NUMBER,MAX_PORTC_PIN_NUMBER,MAX_PORTD_PIN_NUMBER};
+
+static volatile uint8_t previous_gpio_state[DRV_GPIO_PORT_MAX]={0}; 
+
+static volatile drv_gpio_interrupt_mode_t gpio_interrupt_mode_array[DRV_GPIO_PORT_MAX][MAX_PORTB_PIN_NUMBER+1]={0};
+static drv_gpio_callback_t global_gpio_callback=NULL;
+
+static inline void _isr_helper(drv_port_t port){
+    if (global_gpio_callback == NULL) {
+        return ;
+    }
+    uint8_t pin_reg =  *PIN_ARR[port];
+    uint8_t toggled_pins = previous_gpio_state[port]^pin_reg;
+    uint8_t change_pin = 0;
+    
+    for (size_t i = 0; i < sizeof(toggled_pins) * 8; i++)  //converting bytes to bits
+    {
+        if (READ_BIT(toggled_pins,i))
+        {
+            change_pin = i;
+        }
+        else
+        {
+            continue;
+        }
+        
+        switch (gpio_interrupt_mode_array[port][change_pin])
+        {
+        case DRV_GPIO_INTERRUPT_MODE_BOTH_EDGES:
+            global_gpio_callback(port, change_pin,READ_BIT(pin_reg,change_pin));
+            break;
+    
+        case DRV_GPIO_INTERRUPT_MODE_RISING_EDGE:
+            if (READ_BIT(pin_reg,change_pin))
+            {
+                global_gpio_callback(port, change_pin,DRV_GPIO_STATE_HIGH);
+            }
+            break;
+        case DRV_GPIO_INTERRUPT_MODE_FALLING_EDGE:
+            if (!READ_BIT(pin_reg,change_pin))        {
+                global_gpio_callback(port, change_pin,DRV_GPIO_STATE_LOW);
+            }
+            break;
+    
+        default:
+            break;
+        }
+    }
+    
+    previous_gpio_state[port] = pin_reg;
+}
+
+ISR(PCINT0_vect){
+    _isr_helper(DRV_GPIO_PORT_B);
+}
+
+ISR(PCINT1_vect){
+    _isr_helper(DRV_GPIO_PORT_C);
+}
+
+ISR(PCINT2_vect){
+    _isr_helper(DRV_GPIO_PORT_D);
+}
 
 
 drv_gpio_error_t drv_gpio_init(drv_gpio_config_t* gpio_config){
@@ -13,6 +77,13 @@ drv_gpio_error_t drv_gpio_init(drv_gpio_config_t* gpio_config){
     }
     if (gpio_config->pin>MAX_PORT_PIN_NUMBER[gpio_config->port]){
         return DRV_GPIO_ERROR_INVALID_PIN;
+    }
+    
+    if (gpio_config->interrupt_config.enable==true)
+    {
+        SET_BIT(*(PCMSK_ARR[gpio_config->port]),gpio_config->pin);
+        SET_BIT(PCICR,gpio_config->port);
+        gpio_interrupt_mode_array[gpio_config->port][gpio_config->pin]=gpio_config->interrupt_config.drv_gpio_interrupt_mode;
     }
     switch (gpio_config->mode)
     {
@@ -25,16 +96,28 @@ drv_gpio_error_t drv_gpio_init(drv_gpio_config_t* gpio_config){
             SET_BIT(*(PORT_ARR[gpio_config->port]),gpio_config->pin);
             break;
         case DRV_GPIO_MODE_OUTPUT:
+            if (gpio_config->inital_state == DRV_GPIO_STATE_HIGH)
+            {
+                SET_BIT(*(PORT_ARR[gpio_config->port]),gpio_config->pin);
+            }
+            else if (gpio_config->inital_state == DRV_GPIO_STATE_LOW)
+            {
+               CLEAR_BIT(*(PORT_ARR[gpio_config->port]),gpio_config->pin); 
+            }
+            else{
+                return DRV_GPIO_ERROR_INVALID_STATE;
+            }
             SET_BIT(*(DDR_ARR[gpio_config->port]),gpio_config->pin);
             break;    
     default:
         return DRV_GPIO_ERROR_INVALID_MODE;
     }
+    previous_gpio_state[gpio_config->port] = *PIN_ARR[gpio_config->port];
     return DRV_GPIO_SUCCESS;
 }
 
 drv_gpio_error_t drv_gpio_deinit(drv_gpio_config_t* gpio_config){
-    gpio_config->mode=DRV_GPIO_MODE_INPUT_HIGH_Z;
+    gpio_config->mode=DRV_GPIO_MODE_INPUT_PULLUP;
     drv_gpio_error_t error=drv_gpio_init(gpio_config);
     return error;
 }
@@ -61,18 +144,14 @@ drv_gpio_error_t drv_gpio_toggle_output(drv_gpio_config_t* gpio_config){
     return DRV_GPIO_SUCCESS;
 }
 drv_gpio_error_t drv_gpio_get_state(drv_gpio_config_t* gpio_config,drv_gpio_state_t* state){
-    if (gpio_config->mode==DRV_GPIO_MODE_OUTPUT)
-    {
-        *state = READ_BIT(*(PORT_ARR[gpio_config->port]),gpio_config->pin);
-    }
-    else if (gpio_config->mode==DRV_GPIO_MODE_INPUT_PULLUP || gpio_config->mode==DRV_GPIO_MODE_INPUT_HIGH_Z)
-    {
-        *state = READ_BIT(*(PIN_ARR[gpio_config->port]),gpio_config->pin);
-    }
-    else{
-        return DRV_GPIO_ERROR_INVALID_MODE;
-    }
+    *state = READ_BIT(*(PIN_ARR[gpio_config->port]),gpio_config->pin);
     return DRV_GPIO_SUCCESS;
 }
+
+drv_gpio_error_t drv_gpio_register_interrupt_callback(drv_gpio_callback_t callback){
+    global_gpio_callback = callback;
+    return DRV_GPIO_SUCCESS;
+}
+
 
 
--- drivers/gpio/gpio.h
+++ drivers/gpio/gpio.h
@@ -2,8 +2,11 @@
 #define GPIO_H
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "common/helper_macros.h"
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #define MAX_PORTB_PIN_NUMBER 7
 #define MAX_PORTC_PIN_NUMBER 6
@@ -35,16 +38,35 @@ typedef enum drv_gpio_error_e{
     DRV_GPIO_ERROR_INVALID_PIN,
     DRV_GPIO_ERROR_INVALID_MODE,
     DRV_GPIO_ERROR_INVALID_STATE,
-
 }drv_gpio_error_t;
 
-typedef struct{
+typedef enum drv_gpio_interrupt_mode_e{
+    DRV_GPIO_INTERRUPT_MODE_NONE,
+    DRV_GPIO_INTERRUPT_MODE_RISING_EDGE,
+    DRV_GPIO_INTERRUPT_MODE_FALLING_EDGE,
+    DRV_GPIO_INTERRUPT_MODE_BOTH_EDGES,
+    DRV_GPIO_INTERRUPT_MODE_MAX,
+}drv_gpio_interrupt_mode_t;
+
+typedef void (*drv_gpio_callback_t)(drv_port_t,uint8_t,drv_gpio_state_t);
+
+typedef struct drv_gpio_interrupt_config_s
+{
+    bool enable;
+    drv_gpio_interrupt_mode_t drv_gpio_interrupt_mode;
+}drv_gpio_interrupt_config_t;
+
+
+typedef struct drv_gpio_config_s{
     drv_port_t port;   
-    uint8_t pin;        //eg. for PB5 port = DRV_GPIO_PORT_B , pin = 5
+    uint8_t pin;                   //eg. for PB5 port = DRV_GPIO_PORT_B , pin = 5
     drv_mode_t mode;
+    drv_gpio_state_t inital_state; //initial state of the pin, only valid if mode is output
+    drv_gpio_interrupt_config_t interrupt_config;
 }drv_gpio_config_t;
 
 drv_gpio_error_t drv_gpio_init(drv_gpio_config_t* gpio_config);
+drv_gpio_error_t drv_gpio_register_interrupt_callback(drv_gpio_callback_t callback);
 drv_gpio_error_t drv_gpio_deinit(drv_gpio_config_t* gpio_config);
 drv_gpio_error_t drv_gpio_set_output(drv_gpio_config_t* gpio_config);
 drv_gpio_error_t drv_gpio_clear_output(drv_gpio_config_t* gpio_config);
