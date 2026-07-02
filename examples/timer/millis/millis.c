682a44d5b87816c8263c0ccb14f3fc4b6038b64c
PozhilanKarthikeyan <pozhilankarthikeyan2005@gmail.com>
2026-07-02 21:11:39 +0530

    fixing issues in the example

--- examples/timer/millis/millis.c
+++ examples/timer/millis/millis.c
@@ -1,6 +1,8 @@
 #include "timer/timer.h"
 #include "gpio/gpio.h"
 #include "stdint.h"
+#include "avr/interrupt.h"
+#include <stdlib.h>
 
 volatile uint32_t millis_tick=0;
 
@@ -13,12 +15,13 @@ drv_timer_config_t timer_config={
     .device=DRV_TIMER_0,
     .clk_src=DRV_TIMER_CLK_IO_DIV_64,
     .timer_mode=DRV_TIMER_MODE_CTC,
-    .overflow_interrupt_enable=true,
-    .overflow_callback=timer_callback,
+    .overflow_interrupt_enable=false,
+    .overflow_callback=NULL,
     .OCA_config={
-        .output_cmp_value=100, // total count is 250 , i.e 0-249
+        .output_cmp_value=249, // total count is 250 , i.e 0-249
         .output_cmp_mode=DRV_TIMER_OUTPUT_CMP_PIN_DISCONNECTED,
-        .interrupt_enable=false,
+        .interrupt_enable=true,
+        .callback=timer_callback,
     },
     .OCB_config={
         .interrupt_enable=false,
@@ -44,14 +47,15 @@ int main(){
     drv_timer_init(&timer_config);
     drv_gpio_init(&led_config);
     drv_timer_start(&timer_config);
+    sei();
     uint32_t previous_millis_tick=0;
     while (1)
     {
         if ((millis()-previous_millis_tick)>=1000U) //one second
         {
             drv_gpio_toggle_output(&led_config);
+            previous_millis_tick=millis();
         }
-        previous_millis_tick=millis();
         
     }
     
